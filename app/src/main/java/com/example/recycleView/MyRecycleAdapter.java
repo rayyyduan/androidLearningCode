ae3cd451f042ee0b69eee0e0f3a30e7e477fe920
ruiduan <ruiduan@tencent.com>
2021-06-07 14:32:02 +0800

    optimize import

--- app/src/main/java/com/example/recycleView/MyRecycleAdapter.java
+++ app/src/main/java/com/example/recycleView/MyRecycleAdapter.java
@@ -1,16 +1,7 @@
 package com.example.recycleView;
 
-import static android.app.AppGlobals.getPackageManager;
-import static android.webkit.WebViewZygote.getPackageName;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.pm.IPackageManager;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.os.UserHandle;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
